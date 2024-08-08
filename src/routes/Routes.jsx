3fef1d098462d0172842326d467498186d3dd3ce
MritulAich <mridulaich332@gmail.com>
2024-08-08 11:58:37 +0600

    added errorpage

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,10 +2,12 @@ import {
     createBrowserRouter,
 } from "react-router-dom" ;
 import Root from "./layout/Root";
+import Error from "../shared/Error";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        errorElement: <Error></Error>
     },
 ]);
